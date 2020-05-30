b41b5d3861ffd7ee2c78ae8da9785e669afddb72
antor92 <fkcantor94@gmail.com>
2020-05-30 23:02:19 +0600

    Add files via upload

---
+++ TASK-6/bank.java
@@ -0,0 +1,18 @@
+public class bank {
+    
+    public void Balance(int value1, int value2) {
+        int sum = value1 + value2;
+        System.out.println("\n=======================||BALANCE||========================");
+        System.out.println("Summation of Integer: "+sum);
+    }
+    public void Balance(float value1, float value2) {
+        float sum = value1 + value2;
+        System.out.println("Summation of Float type: "+sum);
+    }
+    public void Balance(double value1, double value2) {
+        double sum =value1 * value2;
+        System.out.println("Multipication Of Double type: "+sum);
+    }
+
+
+}
---
+++ TASK-6/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main( String[] args) {
+        bank Bank = new bank();
+        Bank.Balance(8,8);
+        Bank.Balance((float)6.08,(float)5.90);
+        Bank.Balance(7.99,6.08);
+    }
+}
